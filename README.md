19de0142d859233761b872c7fc1048e138fe2036
Iipin <52832022+Iipin@users.noreply.github.com>
2023-05-18 00:33:32 +0200

    Migrate document to YAML with explicit chapter/paragraph/list numbering

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # statute-pl
 [![Build Status](https://travis-ci.com/riichi/statute-pl.svg?branch=master)](https://travis-ci.com/riichi/statute-pl)
+
+## How to build the document locally
+
+1. Install [poetry](https://python-poetry.org/)
+2.
+```shell
+cd renderer
+poetry install
+poetry run main.py ../statut.yaml ../template.tex > out.tex
+pdflatex out.tex
+```
---
+++ renderer/main.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+import sys
+from pathlib import Path
+from typing import Iterable
+
+from pydantic import BaseModel
+from yaml import CLoader, load
+
+
+class Text(BaseModel):
+    text: str
+
+
+class ListElement(BaseModel):
+    id: str
+    content: list[ContentAtom]
+
+
+class List(BaseModel):
+    list: list[ListElement]
+
+
+class Paragraph(BaseModel):
+    id: str
+    content: list[ContentAtom]
+
+
+ContentAtom = List | Text
+
+ListElement.update_forward_refs()
+Paragraph.update_forward_refs()
+
+
+class Chapter(BaseModel):
+    id: str
+    title: str
+    paragraphs: list[Paragraph]
+
+
+class Statute(BaseModel):
+    chapters: list[Chapter]
+
+
+def read_statute(filename: str) -> Statute:
+    with open(filename) as input_file:
+        data = load(input_file, Loader=CLoader)
+    return Statute.parse_obj(data)
+
+
+def render_atom(atom: ContentAtom) -> Iterable[str]:
+    if isinstance(atom, Text):
+        yield atom.text
+        return
+    yield "\\begin{enumerate}"
+    for item in atom.list:
+        yield f"\\item[{item.id}.]"
+        for atom in item.content:
+            yield from render_atom(atom)
+    yield "\\end{enumerate}"
+
+
+def render_paragraph(paragraph: Paragraph) -> Iterable[str]:
+    yield from [
+        "",
+        f"\\renewcommand\\thesubsubsection{{\\S~{paragraph.id}}}",
+        f"\\subsubsection{{}}",
+        "",
+    ]
+    for atom in paragraph.content:
+        yield from render_atom(atom)
+
+
+def render_chapter(chapter: Chapter) -> Iterable[str]:
+    yield from [
+        "",
+        f"\\renewcommand\\thesection{{{chapter.id}}}",
+        f"\\section{{{chapter.title}}}",
+        "",
+    ]
+    for paragraph in chapter.paragraphs:
+        yield from render_paragraph(paragraph)
+
+
+def render(statute: Statute) -> str:
+    lines = []
+    for chapter in statute.chapters:
+        lines.extend(render_chapter(chapter))
+    return "\n".join(lines)
+
+
+def main() -> None:
+    statute = read_statute(sys.argv[1])
+    rendered_statute = render(statute)
+    template = Path(sys.argv[2]).read_text()
+    print(template.replace("$body$", rendered_statute))
+
+
+if __name__ == "__main__":
+    main()
